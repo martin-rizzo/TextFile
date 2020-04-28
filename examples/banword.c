2e1bc7fdd78ee7709e33db4eccdf061fed5e5a34
Martin Rizzo <martinrizzo@gmail.com>
2020-04-28 17:09:32 -0300

    style: improve directory names, ex: 'app' -> 'examples'

---
+++ examples/banword.c
@@ -0,0 +1,13 @@
+
+
+
+/**
+ * Application starting point
+ * @param argc  The number of elements in the 'argv' array
+ * @param argv  An array containing each command-line parameter (starting at argv[1])
+ */
+int main(int argc, char *argv[]) {
+    argc = argc;
+    argv = argv;
+    return 0;
+}
